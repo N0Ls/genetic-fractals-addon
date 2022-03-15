091135e2ecbf73ba9531ad1821b3e9413e991939
N0Ls <nilsland31@gmail.com>
2022-03-16 00:20:43 +0100

    [ui] Rename and move some UI elements

--- ui.py
+++ ui.py
@@ -5,6 +5,27 @@ from .properties import FractalOperators
 from .fractal_nodes import FractalNodesOperators
 
 
+class GenerationPanel(Panel):
+    bl_label = "Generation"
+    bl_idname = "pt.generation_panel"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Fractal'
+
+    def draw(self, context):
+        layout = self.layout
+        row0 = layout.row()
+        row0.operator('op.fractal_operators',
+                      text='Do some crazy stuff').action = 'GENERATE_FRACTAL_SETUP'
+
+        if FractalOperators.fractals_pool.exists(context.collection):
+            row0.enabled = False
+
+        row2 = layout.row()
+        row2.operator('op.fractal_operators',
+                      text='Reset fractals').action = 'RESET_FRACTALS'
+
+
 bpy.types.Scene.fractal_1_like = FloatProperty(
     name="Fractal 1",
     description="Some tooltip",
@@ -30,34 +51,9 @@ bpy.types.Scene.fractal_4_like = FloatProperty(
     min=0.0, max=1.0, step=1, precision=2)
 
 
-class GenerationPanel(Panel):
-    bl_label = "Generation"
-    bl_idname = "pt.generation_panel"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = 'Fractal'
-
-    def draw(self, context):
-        layout = self.layout
-        row0 = layout.row()
-        row0.operator('op.fractal_operators',
-                      text='Do some crazy stuff').action = 'GENERATE_FRACTAL_SETUP'
-
-        if FractalOperators.fractals_pool.exists(context.collection):
-            row0.enabled = False
-
-        row2 = layout.row()
-        row2.operator('op.fractal_operators',
-                      text='Reset fractals').action = 'RESET_FRACTALS'
-
-        row4 = layout.row()
-        row4.operator('op.fractal_operators',
-                      text='Next iteration').action = 'COMPUTE_NEXT_ITERATION'
-
-
-class PropertiesPanel(Panel):
-    bl_label = "Properties"
-    bl_idname = "pt.properties_panel"
+class GeneticPanel(Panel):
+    bl_label = "Genetic"
+    bl_idname = "pt.genetic_panel"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = 'Fractal'
@@ -65,18 +61,24 @@ class PropertiesPanel(Panel):
     def draw(self, context):
         layout = self.layout
         sceneCtx = context.scene
-        # draw the checkbox (implied from property type = bool)
+
         layout.prop(sceneCtx, "fractal_1_like")
         layout.prop(sceneCtx, "fractal_2_like")
         layout.prop(sceneCtx, "fractal_3_like")
         layout.prop(sceneCtx, "fractal_4_like")
 
+        layout.separator()
+
+        nextItRow = layout.row()
+        nextItRow.operator('op.fractal_operators',
+                           text='Next iteration').action = 'COMPUTE_NEXT_ITERATION'
+
 
 def register():
     bpy.utils.register_class(GenerationPanel)
-    bpy.utils.register_class(PropertiesPanel)
+    bpy.utils.register_class(GeneticPanel)
 
 
 def unregister():
     bpy.utils.unregister_class(GenerationPanel)
-    bpy.utils.unregister_class(PropertiesPanel)
+    bpy.utils.unregister_class(GeneticPanel)
