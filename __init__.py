0d647dfc72b818e5e22d50bd6a49112b502321ea
N0Ls <nilsland31@gmail.com>
2022-03-19 20:48:54 +0100

    [info] Update addon infos

--- __init__.py
+++ __init__.py
@@ -1,18 +1,15 @@
-import bpy
 bl_info = {
     "name": "Genetic fractals",
     "description": "Algo genetic powered fractals generator for Blender.",
     "author": "",
     "version": (1, 0),
-    "blender": (3, 0, 1),
-    "location": "",
+    "blender": (3, 0, 0),
+    "location": "View3D > N",
+    "category": "View 3D",
     "warning": "This version is still a work in progress.",
-    "wiki_url": "",
-    "tracker_url": "",
-    "support": "",
-    "category": ""
 }
 
+import bpy
 
 def register():
     from . import properties
